f722343eb9e6f376f16324431e1192c34d00a5e5
Carlos Eduardo Escalante Montilla <129804004+CarlosEscalanteM@users.noreply.github.com>
2024-02-15 04:01:03 +0000

    First Attempt

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,66 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+    const [tasks, setTasks] = useState([]);
+    const [newTask, setNewTask] = useState("");
+    useEffect(() => {
+      const fetchTasks = async () => {
+            try {
+                const response = await fetch(
+                    "https://assets.breatheco.de/apis/fake/todos/user/jimmyhaha"
+                );
+                const body = await response.json();
+                setTasks(body);
+            } catch (error) {
+                alert(error);
+            }
+        };
+        fetchTasks();
+    }, []); 
+    const addTaskToApi = async () => {
+        
+        const newTaskObject = {
+            done: false,
+            label: newTask
+        };
+    const updatedTasks = [...tasks, newTaskObject];
+        setTasks(updatedTasks);
+    
+        try {
+            await fetch(
+                "https://assets.breatheco.de/apis/fake/todos/user/jimmyhaha", {
+                    method: "PUT",
+                    body: JSON.stringify(updatedTasks),
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                }
+            );
+        } catch (error) {
+            alert(error);
+        }
+    };
+    return (
+        <div className='text-center'>
+            <h1 className="text-center">
+                <ul>
+                    {tasks.map((task) => (
+                        <li key={task.id}>
+                            {task.label}
+                        </li>
+                    ))}
+                </ul>
+                <input
+                    type="text"
+                    value={newTask}
+                    onChange={(e) => setNewTask(e.target.value)}
+                />
+                <button onClick={addTaskToApi}>
+                    Add Task
+                </button>
+            </h1>
+        </div>
+    );
 };
-
 export default Home;
